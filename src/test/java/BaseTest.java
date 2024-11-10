b569b318a6cfebf82b6cb13d3ce8454c24ecea21
Vitalii <virthunter@gmail.com>
2024-11-10 10:46:04 +0200

    added Wait.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,23 @@
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseTest {
+    ChromeDriver driver;
+    String BASE_URL = "https://www.idealo.de/";
+
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get(BASE_URL);
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
 }
